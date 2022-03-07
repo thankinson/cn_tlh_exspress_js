2819002bf9f87d5b49ad3907c3d3fe7e8037ecd3
thankinson <t_hankinson@outlook.com>
2022-03-07 12:10:07 +0000

    updates

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000; //5001 on mac
 
 app.use("/static", express.static("public"));
-app.use("/css", express.static("public/css"))
+app.use("/css", express.static("public/css"));
 // http://localhost:5000/static/
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
